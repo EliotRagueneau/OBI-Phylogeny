755174c9417b1ee87477a7726ca653747440050d
Orkeliot <eliot.ragueneau@etu.u-bordeaux.fr>
2018-11-16 10:29:25 +0100

    Cured protein sequences

--- cure.py
+++ cure.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 import re
 specie = re.compile(r'[A-Z][a-z]+\s[a-z]{2,}')
-with open("arb-silva.de_2018-11-16_id570591_gapcompressedvertical.fasta") as input_file:
-    with open("cleaned_16S_alignement.fasta", 'w') as output_file:
+with open("protein_sequence.fasta") as input_file:
+    with open("cleaned_protein_sequence.fasta", 'w') as output_file:
         for line in input_file.readlines():
             if line[0] == '>':
                 if specie.search(line):
