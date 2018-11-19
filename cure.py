df2f931345cd92d3bebd0a047a2914059e8f2abd
Orkeliot <eliot.ragueneau@etu.u-bordeaux.fr>
2018-11-19 13:36:58 +0100

    clean new 16S

--- cure.py
+++ cure.py
@@ -2,8 +2,8 @@
 import re
 
 specie = re.compile(r'[A-Z][a-z]+\s[a-z]{2,}')
-with open("raw_protein_sequence.fasta") as input_file:
-    with open("cleaned_protein_sequence.fasta", 'w') as output_file:
+with open("16S/news_16S.fasta") as input_file:
+    with open("16S/cleaned_new_16s.fasta", 'w') as output_file:
         for line in input_file.readlines():
             if line[0] == '>':
                 if specie.search(line):
@@ -11,7 +11,6 @@ with open("raw_protein_sequence.fasta") as input_file:
                     output_file.write('>' + latin + '\n')
                 else:
                     output_file.write(line)
-
 
             else:
                 output_file.write(line.replace(".", "-"))
