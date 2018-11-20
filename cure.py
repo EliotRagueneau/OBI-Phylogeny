a923fb4f51ad2a81566f3b647525f7bb3e499a98
Orkeliot <eliot.ragueneau@etu.u-bordeaux.fr>
2018-11-20 09:44:17 +0100

    clean new 16S

--- cure.py
+++ cure.py
@@ -2,8 +2,8 @@
 import re
 
 specie = re.compile(r'[A-Z][a-z]+\s[a-z]{2,}')
-with open("Phe t-RNA/DNA_pheT.fasta") as input_file:
-    with open("Phe t-RNA/cleaned_DNA_pheT.fasta", 'w') as output_file:
+with open("/home/eliot/ProjetOBIS1/Phe t-RNA/raw_protein_sequence_rooted.fasta") as input_file:
+    with open("/home/eliot/ProjetOBIS1/Phe t-RNA/cleaned_protein_sequence_rooted.fasta", 'w') as output_file:
         for line in input_file.readlines():
             if line[0] == '>':
                 if specie.search(line):
